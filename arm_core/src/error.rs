c61588cfd88cf7600f6c25b7dc019605b9cfc341
agureev <artguril@proton.me>
2026-04-21 23:42:12 +0400

    Add General Error and Wrap Nullifier Error

--- arm_core/src/error.rs
+++ arm_core/src/error.rs
@@ -2,10 +2,20 @@
 #![allow(missing_docs)]
 use thiserror::Error;
 
+use arm_traits::resource::NullifierError;
+
 #[derive(Debug, Error, Clone, PartialEq, Eq)]
 pub enum ArmError {
     #[error("Empty nullifier array for compliance proof")]
     ComplianceProofEmptyNullifierArray,
     #[error("Nonce mismatch for a created resource in compliance proof")]
     ComplianceProofCreatedResourceNonceMismatch,
+    #[error("RM Error")]
+    GeneralError,
+}
+
+impl From<NullifierError> for ArmError {
+    fn from(_: NullifierError) -> Self {
+        ArmError::GeneralError
+    }
 }
